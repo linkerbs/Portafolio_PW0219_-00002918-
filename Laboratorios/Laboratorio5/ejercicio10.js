ec3413f308376b077701bcd20b8a9730b6de2564
UCA <00002918@uca.edu.sv>
2019-09-23 10:55:37 -0600

    Finalizacion de 9 ejercicios (Labo 5)

---
+++ Laboratorios/Laboratorio5/ejercicio10.js
@@ -0,0 +1,13 @@
+var num = -14;
+function absol(nume)
+{
+   if(nume > 0){
+       console.log(nume);
+   }
+   else{
+       numer = -nume;
+       console.log(numer)
+   }
+}
+
+absol(num);
--- Laboratorios/Laboratorio5/ejercicio8.js
+++ Laboratorios/Laboratorio5/ejercicio8.js
@@ -1,15 +1,18 @@
 var num = 14;
 var bin = 0;
-var actual = 0;
+var actual = "";
 function decimal(nume)
 {   
     do{
         bin = num % 2;
-        num = num/2;
+        num = Math.floor(num/2);
+        
         actual = actual + bin;
-    }while(num!=2)
+    }while(num>1)
 
-    console.log(actual);
+    var fin;
+    fin = actual + num;
+   console.log();
    
 }
 
